8e2392df16a40db6388922a43ed31c045cdd973f
ceyylmz <yilmazceyda27@gmail.com>
2026-03-16 11:30:18 +0300

    Klavye girişleri tamamlandı

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("VİZE NOTUNUZU GİRİNİZ");
+     int vize = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("FİNAL NOTUNUZU GİRİNİZ");
+        int final = Convert.ToInt32(Console.ReadLine());
+
+    }
+
+    static void ortalama()
+    {
+        //todo:
+    }
+
+    static void harfnotu()
+    {
+        //todo:
+    }
+}
