b198cde3f84d2aa78abf7217e61700a2fd0ac19c
Furkan6437 <furkanugurlar01@gmail.com>
2026-03-16 11:52:52 +0300

    harfNotunu hesaplandı.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("VİZE NOTUNUZU GİRİNİZ");
-     int vize = Convert.ToInt32(Console.ReadLine());
+        int vize = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("FİNAL NOTUNUZU GİRİNİZ");
         int final = Convert.ToInt32(Console.ReadLine());
@@ -19,6 +19,21 @@ class Program
 
     static void harfnotu()
     {
-        //todo:
+       double ortalama=Convert.ToDouble(Console.ReadLine());
+        string harfNotu = "";
+
+        if (ortalama>0 && ortalama<30)
+        {
+            harfNotu = "FF";
+        }
+        else if (ortalama >=30 && ortalama <60)
+        {
+            harfNotu = "CC";
+        }
+        else if (ortalama >= 60 && ortalama < 100)
+        {
+            harfNotu = "AA";
+        }
+        
     }
 }
